37d3fe0caee434c2f18c1abd519375e8328c3fc0
Rachel Hicken <rachkh22@gmail.com>
2018-03-26 09:26:38 -0600

    5 & 6 working. Assessment complete.

--- assessment.js
+++ assessment.js
@@ -15,24 +15,23 @@ var me = "My name is " + myName + " and my favorite number is " + faveNum;
 
 // #5 Using the previously created faveNum variable, create a new variable called 'trueFaveNum'. *trueFaveNum* should return true when comparing faveNum to your favorite number.v // HINT: Use a comparison operator
 
-//var trueFaveNum;
-//if (trueFaveNum === faveNum)
-//{
-//  return true;
-//}
+
+var trueFaveNum;
+if (faveNum === 22)
+{
+  trueFaveNum = true;
+}
 
 // #6 Create a new variable called notHakeem. Using the previously created myName variable, test that your name DOES NOT equal the string 'Hakeem Olajuwon'. If your name does happen to be Hakeem Olajuwon, test against 'Karl Malone'
 
  var notHakeem;
  if (myName !== "Hakeem Olajuwon") {
-  if (myName === "Karl Malone")
-  {
-    return true;
-  }
+   notHakeem = true;
  } 
-// else if (myName === "Karl Malone") {
-  
-// }
+ else if (myName === "Karl Malone")
+   {
+    notHakeem = "Karl Malone";
+   }
 
 // #7 Create an if statement that checks the age variable below. If age is greater than 16, then set canDrive to true otherwise set canDrive to false. The canDrive variable will be set by the unit test so do not adjust it.
 var age = 18;
